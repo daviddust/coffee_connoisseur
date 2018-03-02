04820aba0213ba42aeffe9d275c21b5704ccd8a2
David Staub <david_staub@hotmail.com>
2018-03-02 21:36:15 +0100

    enabled debug

--- app/main.py
+++ app/main.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hooi David, Hoi Timo'
+    return 'Hooi David, Hoi Timo und test'
+
+if __name__ == '__main__':
+    app.run(debug=True)
